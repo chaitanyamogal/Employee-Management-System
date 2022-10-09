fd0a457672099cbb66332f5842e74daa1260f9b7
chaitanya <chaitanyamogal@gmail.com>
2022-10-09 11:56:42 +0530

    Final commit of basic project

--- src/main/java/com/empsystem/empsystem/controller/EmpController.java
+++ src/main/java/com/empsystem/empsystem/controller/EmpController.java
@@ -1,38 +1,64 @@
 package com.empsystem.empsystem.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.empsystem.empsystem.entity.Employee;
 import com.empsystem.empsystem.service.EmpService;
 
+import java.util.List;
+
 @Controller
 public class EmpController {
-	
+
 	@Autowired
 	private EmpService service;
-	
+
 	@GetMapping("")
-	public String home()
-	{
+	public String home(Model m) {
+		List<Employee> emp = service.getAllEmp();
+		m.addAttribute("emp", emp);
 		return "index";
 	}
-	
+
 	@GetMapping("/add-emp")
-	public String addEmp() 
-	{
+	public String addEmp() {
 		return "add_emp";
 	}
-	
+
 	@PostMapping("/register")
-	public String empRegister(@ModelAttribute Employee e)
-	{
+	public String empRegister(@ModelAttribute Employee e, HttpSession session) {
 		System.out.println(e);
 		service.addEmp(e);
+		session.setAttribute("msg", "Employee Added Successfully...");
+		return "redirect:/";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable int id, Model m) {
+		Employee e = service.getEmployeeId(id);
+		m.addAttribute("emp", e);
+		return "edit_emp";
+	}
+
+	@PostMapping("/update")
+	public String updateEmp(@ModelAttribute Employee e, HttpSession session) {
+		service.addEmp(e);
+		session.setAttribute("msg", "Employee Data updated Successfully");
+		return "redirect:/";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteEmp(@PathVariable int id, HttpSession session) {
+		service.deleteEmp(id);
+		session.setAttribute("msg", "Employee Deleted successfully");
 		return "redirect:/";
 	}
 }
--- src/main/java/com/empsystem/empsystem/service/EmpService.java
+++ src/main/java/com/empsystem/empsystem/service/EmpService.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 import com.empsystem.empsystem.entity.Employee;
 import com.empsystem.empsystem.repository.EmpRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class EmpService {
 	
@@ -15,5 +18,21 @@ public class EmpService {
 	public void addEmp(Employee e) {
 		repo.save(e);
 	}
+	
+	public List<Employee> getAllEmp(){
+		return repo.findAll();
+	}
 
+	public Employee getEmployeeId(int id)
+	{
+		Optional<Employee> e = repo.findById(id);
+		if(e.isPresent()) {
+			return e.get();
+		}
+		return null;
+	}
+	
+	public void deleteEmp(int id) {
+		repo.deleteById(id);
+	}
 }
